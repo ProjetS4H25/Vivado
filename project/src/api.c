fa6c08e9fc40ebb2781e6ea096895116511cc939
Luka Lepine <117468514+DifficultCupid2@users.noreply.github.com>
2025-03-07 15:52:46 -0500

    fichier .c
    -

---
+++ project/src/api.c
@@ -0,0 +1,24 @@
+#ifndef API_C
+#define API_C
+
+void apiMoveViewPort() {
+
+}
+
+void apiMoveActor() {
+
+}
+
+void apiSetBackgroundBuffer() {
+
+}
+
+void apiReset() {
+
+}
+
+void apiSetActorPosition() {
+
+}
+
+#endif
--- project/src/api.h
+++ project/src/api.h
@@ -1,24 +1,10 @@
 #ifndef API_H
 #define API_H
 
-void apiMoveViewPort() {
-
-}
-
-void apiMoveActor() {
-
-}
-
-void apiSetBackgroundBuffer() {
-
-}
-
-void apiReset() {
-
-}
-
-void apiSetActorPosition() {
-
-}
+void apiMoveViewPort();
+void apiMoveActor();
+void apiSetBackgroundBuffer();
+void apiReset();
+void apiSetActorPosition();
 
 #endif
