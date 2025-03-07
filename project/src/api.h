59cdb6c90ddcf947c0428b67ef28ed96d8ad50d6
Luka Lepine <117468514+DifficultCupid2@users.noreply.github.com>
2025-03-07 15:49:33 -0500

    fonctions vide
    -

---
+++ project/src/api.h
@@ -0,0 +1,24 @@
+#ifndef API_H
+#define API_H
+
+void apiMoveViewPort() {
+
+}
+
+void apiMoveActor() {
+
+}
+
+void apiSetBackgroundBuffer() {
+
+}
+
+void apiReset() {
+
+}
+
+void apiSetActorPosition() {
+
+}
+
+#endif
