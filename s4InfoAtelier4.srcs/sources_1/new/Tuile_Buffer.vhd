7e69d5160b88154f2ffac25a44a57da9a09ee493
JoeCharette53 <jordan.charette@usherbrooke.ca>
2025-03-12 18:51:13 -0400

    Ajout du code qui a pas suivi for no reason

--- s4InfoAtelier4.srcs/sources_1/new/Tuile_Buffer.vhd
+++ s4InfoAtelier4.srcs/sources_1/new/Tuile_Buffer.vhd
@@ -1,26 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/12/2025 06:47:44 PM
--- Design Name: 
--- Module Name: Tuile_Buffer - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -32,12 +12,57 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Tuile_Buffer is
---  Port ( );
+  Port ( 
+  -- du système
+  Clk: in std_logic ;
+
+  -- du controleur
+  Ch_tuileId: in std_logic_vector (5 downto 0);
+  Ch_X: in std_logic_vector (2 downto 0);
+  Ch_Y: in std_logic_vector (2 downto 0);
+  Ch_CC: in std_logic_vector (4 downto 0);
+  We_tuile: in std_logic;
+
+  -- Du background
+  tuile_id: in std_logic_vector (5 downto 0);
+  tuile_X: in std_logic_vector (2 downto 0);
+  tuile_Y: in std_logic_vector (2 downto 0);
+
+  -- Sortie
+  Code_Couleur: out std_logic_vector(4 downto 0)
+  );
 end Tuile_Buffer;
 
 architecture Behavioral of Tuile_Buffer is
 
+-- Tableau de tuile
+-- chaque element de la matrice fait 5 bits
+type t_tuile is array (0 to 7, 0 to 7) of std_logic_vector(4 downto 0); --  matrice 8×8 d’une tuile
+type t_memory is array (0 to 63) of t_tuile; -- memorise la valeurs des tuiles
+
+-- initier la valeur du tableaux
+signal mem : t_memory := (others => (others => (others => (others => '0'))));
+
 begin
+-- ecriture si We_tuile = 1
+--  on remplit la mémoire des tuiles avec le controleurs, 
+process(Clk)
+  begin
+    if rising_edge(Clk) then
+      if We_tuile = '1' then
+        mem(to_integer(unsigned(Ch_tuileId)))(
+            to_integer(unsigned(Ch_Y)),
+            to_integer(unsigned(Ch_X))
+        ) <= Ch_CC;
+      end if;
+    end if;
+  end process;
+
+-- Prend la valeur dans la mémoire de tuile et la retourne 
+Code_Couleur <= mem(to_integer(unsigned(tuile_id)))(
+                    to_integer(unsigned(tuile_Y)),
+                    to_integer(unsigned(tuile_X))
+                  );
 
 
 end Behavioral;
