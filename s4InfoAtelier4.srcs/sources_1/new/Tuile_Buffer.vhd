d4ce9390ccccbf7965711e7816eacc6865b8e149
JoeCharette53 <jordan.charette@usherbrooke.ca>
2025-03-12 18:48:45 -0400

    Ajout du tuile buffer

---
+++ s4InfoAtelier4.srcs/sources_1/new/Tuile_Buffer.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/12/2025 06:47:44 PM
+-- Design Name: 
+-- Module Name: Tuile_Buffer - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Tuile_Buffer is
+--  Port ( );
+end Tuile_Buffer;
+
+architecture Behavioral of Tuile_Buffer is
+
+begin
+
+
+end Behavioral;
