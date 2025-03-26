201b18a11cfc0eb0ede691997bfb3fc2ed57f66d
Vincent <forv1401@usherbrooke.ca>
2025-03-26 09:49:28 -0400

    Manager combinatoire

---
+++ s4InfoAtelier4.srcs/sim_1/new/tb_actor_manager.vhd
@@ -0,0 +1,119 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_actor_manager is
+--  Port ( );
+end tb_actor_manager;
+
+architecture Behavioral of tb_actor_manager is
+
+
+component actor_manager
+    PORT (
+        clk:        in std_logic;
+        i_moveEnable: in std_logic; -- flag pour bouger avec un delta
+        i_initPosition:   in std_logic; -- flag pour set la position/tuile id
+        i_initTuile:      in std_logic; -- flag pour set la position/tuile id
+        i_tuileId:  in std_logic_vector(11 downto 0); -- les deux tuiles dans un signal
+        i_globalX:    in std_logic_vector(9 downto 0);
+        i_globalY:    in std_logic_vector(9 downto 0);
+        i_offsetX:    in std_logic_vector(5 downto 0);
+        i_offsetY:    in std_logic_vector(5 downto 0);
+        i_positionX:  in std_logic_vector(5 downto 0);
+        i_positionY:  in std_logic_vector(5 downto 0);
+        i_actor_id:   in std_logic_vector(3 downto 0);
+        o_tuile_id:   out std_logic_vector(5 downto 0);
+        o_tuile_X:    out std_logic_vector(2 downto 0);
+        o_tuile_Y:    out std_logic_vector(2 downto 0)
+          );
+end component;
+
+
+-- l'horloge devrait être 50 MHz
+   signal   d_clk_p       :  std_logic := '0';   -- (sol) horloge principale 50 MHz (utile pour cette simulation a éviter si possible)
+   signal   d_reset       :  std_logic := '0';
+   signal   s_glbX : STD_LOGIC_VECTOR (9 downto 0) := (others => '0');
+   signal   s_glbY : STD_LOGIC_VECTOR (9 downto 0) := (others => '0');
+   signal   o_tuileId : STD_LOGIC_VECTOR (5 downto 0) := (others => '0');
+   signal   o_tuileX : STD_LOGIC_VECTOR (2 downto 0) := (others => '0');
+   signal   o_tuileY : STD_LOGIC_VECTOR (2 downto 0) := (others => '0');
+
+   -- signal test pour vérification
+   signal expected_status_code    : std_logic_vector (1 downto 0);
+
+   constant c_clk_p_Period      : time :=  20 ns;  -- 50 MHz  -- frequence de l'horloge utilisee pour module_commande dans la problématique
+   constant c_delai_commandes   : time :=  10 us;  -- delai entre commandes du bouton
+   CONSTANT PERIOD    : time := 10 ns;
+   
+    signal s_moveEnable:    std_logic;
+    signal s_initPosition:  std_logic;
+    signal s_initTuile:     std_logic;
+    signal s_tuileId:       std_logic_vector(11 downto 0) := (others => '0');
+    signal s_tuile_X:       std_logic_vector(2 downto 0) := (others => '0');
+    signal s_tuile_y:       std_logic_vector(2 downto 0) := (others => '0');
+    signal s_offset_x:      std_logic_vector(5 downto 0) := (others => '0');
+    signal s_offset_y:      std_logic_vector(5 downto 0) := (others => '0');
+    signal s_positionX:     std_logic_vector(5 downto 0);
+    signal s_positionY:     std_logic_vector(5 downto 0);
+    signal s_actor_id:      std_logic_vector(3 downto 0);
+   
+begin
+
+   ----------------------------------------------------------------------------
+   -- unites objets du test  
+   ----------------------------------------------------------------------------
+     
+ UUT: actor_manager
+ Port map
+    (       
+      clk               => d_clk_p,
+      i_globalX         => s_glbX,
+      i_globalY         => s_glbY,
+      i_moveEnable      => s_moveEnable,
+      i_initPosition    => s_initPosition,
+      i_initTuile       => s_initTuile,
+      i_tuileId         => s_tuileId,
+      i_offsetX         => s_offset_x,
+      i_offsetY         => s_offset_y,
+      i_positionX       => s_positionX,
+      i_positionY       => s_positionY,
+      i_actor_id        => s_actor_id,
+      o_tuile_id        => o_tuileId,
+      o_tuile_X         => o_tuileX,
+      o_tuile_Y         => o_tuileY
+  );
+
+   ----------------------------------------------------------------------------
+   -- generation horloge  
+   ----------------------------------------------------------------------------
+   
+  sim_clk_p:  process
+   begin
+      d_clk_p <= '1';  -- init
+      loop
+         wait for c_clk_p_Period / 2;
+         d_clk_p <= not d_clk_p; -- invert clock value
+      end loop;
+   end process;  
+
+tb : PROCESS
+   BEGIN      
+      wait for PERIOD; s_glbX <="0000000000"; s_glbY <="0000000000";
+      wait for PERIOD; wait for PERIOD; 
+      
+      wait for PERIOD; s_glbX <="0000010000"; s_glbX <="0000010000"; s_positionX <= "0000010000"; s_positionY <= "0000010000"; s_initPosition <= '1';
+      wait for PERIOD; wait for PERIOD; 
+      
+      WAIT; -- will wait forever
+ end process;
+ 
+end Behavioral;
--- s4InfoAtelier4.srcs/sources_1/new/actor_manager.vhd
+++ s4InfoAtelier4.srcs/sources_1/new/actor_manager.vhd
@@ -34,74 +34,108 @@ use IEEE.NUMERIC_STD.ALL;
 entity actor_manager is
     Port ( 
         clk:        in std_logic;
-        reset:      in std_logic;
-        moveEnable: in std_logic; -- flag pour bouger avec un delta
-        initPositionEn:   in std_logic; -- flag pour set la position/tuile id
-        initTuileEn:      in std_logic; -- flag pour set la position/tuile id
-        initTuile:  in std_logic_vector(11 downto 0); -- les deux tuiles dans un signal
-        globalX:    in std_logic_vector(9 downto 0);
-        globalY:    in std_logic_vector(9 downto 0);
-        offsetX:    in std_logic_vector(5 downto 0);
-        offsetY:    in std_logic_vector(5 downto 0);
-        positionX:  in std_logic_vector(5 downto 0);
-        positionY:  in std_logic_vector(5 downto 0);
-        actor_id:   in std_logic_vector(3 downto 0);
-        tuile_id:   out std_logic_vector(5 downto 0);
-        tuile_X:    out std_logic_vector(2 downto 0);
-        tuile_Y:    out std_logic_vector(2 downto 0)
+        i_moveEnable: in std_logic; -- flag pour bouger avec un delta
+        i_initPosition:   in std_logic; -- flag pour set la position/tuile id
+        i_initTuile:      in std_logic; -- flag pour set la position/tuile id
+        i_tuileId:  in std_logic_vector(11 downto 0); -- les deux tuiles dans un signal
+        i_globalX:    in std_logic_vector(9 downto 0);
+        i_globalY:    in std_logic_vector(9 downto 0);
+        i_offsetX:    in std_logic_vector(5 downto 0);
+        i_offsetY:    in std_logic_vector(5 downto 0);
+        i_positionX:  in std_logic_vector(5 downto 0);
+        i_positionY:  in std_logic_vector(5 downto 0);
+        i_actor_id:   in std_logic_vector(3 downto 0);
+        o_tuile_id:   out std_logic_vector(5 downto 0);
+        o_tuile_X:    out std_logic_vector(2 downto 0);
+        o_tuile_Y:    out std_logic_vector(2 downto 0)
    
     );
 end actor_manager;
     
 architecture Behavioral of actor_manager is
---Insert the following in the architecture before the begin keyword
-   --Use descriptive names for the states, like st1_reset, st2_search
-   type state_type is (idle, SetPositionX, SetPositionY, ChangePositionX, ChangePositionY, ChangeTuileId);
-   signal state, next_state : state_type;
-   --other outputs
+    component actor is
+      Port ( 
+             clk       : in std_logic;
+             i_initTuile : in std_logic; -- flag pour set les tuileID
+             i_initPos : in std_logic; -- flag pour set la position
+             i_tuileId1 : in std_logic_vector (5 downto 0); -- pour set le tuileId
+             i_tuileId2 : in std_logic_vector (5 downto 0); -- pour set le tuileId
+             i_globalX : in std_logic_vector (9 downto 0); -- postion x global
+             i_globalY : in std_logic_vector (9 downto 0); -- postion y global
+             i_moveOffsetX : in std_logic_vector (5 downto 0); -- delta du deplacement X
+             i_moveOffsetY : in std_logic_vector (5 downto 0); -- delta du deplacement Y
+             i_moveEnable : in std_logic; -- permet de bouger actor
+             o_tuileX : out std_logic_vector (2 downto 0); -- postion x du pixel dans la tuile
+             o_tuileY : out std_logic_vector (2 downto 0); -- position y du pixel dans la tuile
+             o_tuileId : out std_logic_vector (5 downto 0) -- tuile de l'acteur
+      );
+  end component;
     
-    signal moveEn1: std_logic;
-    signal moveEn2: std_logic;
+    signal s_moveEn1: std_logic;
+    signal s_moveEn2: std_logic;
+    signal s_initTuile1: std_logic;
+    signal s_initTuile2: std_logic;    
+    signal s_initPos1: std_logic;
+    signal s_initPos2: std_logic;
     
-    signal s_offset_x:      std_logic_vector(9 downto 0) := (others => '0');
-    signal s_offset_y:      std_logic_vector(9 downto 0) := (others => '0');
+    signal s_tuileX1:   std_logic_vector(2 downto 0);
+    signal s_tuileX2:   std_logic_vector(2 downto 0);
+    
+    signal s_tuileY1:   std_logic_vector(2 downto 0);
+    signal s_tuileY2:   std_logic_vector(2 downto 0);
+    
+    signal s_tuileId1:  std_logic_vector(5 downto 0);
+    signal s_tuileId2:  std_logic_vector(5 downto 0);
+    
     signal s_global_x:      std_logic_vector(9 downto 0) := (others => '0');
     signal s_global_y:      std_logic_vector(9 downto 0) := (others => '0');
-    signal s_tuile_id:      std_logic_vector(9 downto 0) := (others => '0');
+    
+    signal s_initTuile:     std_logic_vector(11 downto 0);
+    signal s_tuileId:       std_logic_vector(5 downto 0) := (others => '0');
+    signal s_tuile_X:       std_logic_vector(2 downto 0) := (others => '0');
+    signal s_tuile_y:       std_logic_vector(2 downto 0) := (others => '0');
+    signal s_offset_x:      std_logic_vector(5 downto 0) := (others => '0');
+    signal s_offset_y:      std_logic_vector(5 downto 0) := (others => '0');
+    
+    
 
 begin
 
-
+    actor1: component actor
+    Port map(
+        clk             => clk,
+        i_initTuile     => s_initTuile1,
+        i_initPos       => s_initPos1,
+        i_moveEnable    => s_moveEn1,
+        i_tuileId1      => s_initTuile(11 downto 6),
+        i_tuileId2      => s_initTuile(5 downto 0),
+        i_globalX       => s_global_x,
+        i_globalY       => s_global_y,
+        i_moveOffsetX   => s_offset_x,
+        i_moveOffsetY   => s_offset_y,
+        o_tuileX        => s_tuileX1,
+        o_tuileY        => s_tuileY1,
+        o_tuileId       => s_tuileId
+        
+    );
     
     
---Insert the following in the architecture after the begin keyword
-   SYNC_PROC: process (clk)
-   begin
-      if (clk'event and clk = '1') then
-         if (reset = '1') then
-            
-         else if (initTuile = '1') then
-         
-         else if (initPosition = '1') then
-            
-         else if (moveEnable = '1') then
-            case actor_id is
-                when "0000" =>
-                    s_a1_en <= '1';
-                    -- set les output de a1
-                    s_a2_en <= '0';
-                    
-                when "0001" =>
-                    s_a1_en <= '0';
-                    s_a2_en <= '1';
-   
-                when others =>
-                    s_a1_en <= '0';
-                    s_a2_en <= '0';
-                    s_tuile_id <= (others => '0');
-             end case;
-         end if;
-      end if;
-   end process;
+  s_moveEn1 <= '1' when (i_moveEnable = '1' AND i_actor_id = "0000") else '0';
+  s_moveEn2 <= '1' when (i_moveEnable = '1' AND i_actor_id = "0001") else '0';
+  
+  s_initPos1    <= '1' when (i_initPosition = '1' AND i_actor_id = "0000") else '0';           
+  s_initPos2    <= '1' when (i_initPosition = '1' AND i_actor_id = "0001") else '0';
 
+  s_initTuile1    <= '1' when (i_initTuile = '1' AND i_actor_id = "0000") else '0';
+  s_initTuile2    <= '1' when (i_initTuile = '1' AND i_actor_id = "0001") else '0';
+  
+  s_tuile_x <= s_tuileX1 when (s_tuileId1 /= "000000") else 
+                s_tuileX2 when (s_tuileId2 /= "000000") else "000000";
+                
+  s_tuile_y <= s_tuileY1 when (s_tuileId1 /= "000000") else 
+                s_tuileY2 when (s_tuileId2 /= "000000") else "000000";
+                
+  s_tuileId <= s_tuileId1 when (s_tuileId1 /= "000000") else
+                s_tuileId2 when (s_tuileId2 /= "000000") else "000000";
+    
 end Behavioral;
