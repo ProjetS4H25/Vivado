a49420038b9e056dd551517b60e3bf43194b641a
JoeCharette53 <jordan.charette@usherbrooke.ca>
2025-03-27 15:32:53 -0400

    Corriger le viewport + changer le background pour qu'il fonctionne avec 8 bits de global

--- s4InfoAtelier4.srcs/sim_1/new/tb_viewport.vhd
+++ s4InfoAtelier4.srcs/sim_1/new/tb_viewport.vhd
@@ -41,23 +41,23 @@ component viewport is
     Port ( clk : in STD_LOGIC;
            reset: in STD_LOGIC;
            enable: in STD_LOGIC;
-           pp_x : in STD_LOGIC_VECTOR (9 downto 0);
-           pp_y : in STD_LOGIC_VECTOR (9 downto 0);
+           pp_x : in STD_LOGIC_VECTOR (8 downto 0);
+           pp_y : in STD_LOGIC_VECTOR (8 downto 0);
            offset_x : in STD_LOGIC_VECTOR (5 downto 0);
            offset_y : in STD_LOGIC_VECTOR (5 downto 0);
-           global_x : out STD_LOGIC_VECTOR (9 downto 0);
-           global_y : out STD_LOGIC_VECTOR (9 downto 0));
+           global_x : out STD_LOGIC_VECTOR (8 downto 0);
+           global_y : out STD_LOGIC_VECTOR (8 downto 0));
 end component;
 
 signal clk : STD_LOGIC;
 signal reset: STD_LOGIC;
 signal enable : STD_LOGIC;
-signal pp_x : STD_LOGIC_VECTOR (9 downto 0);
-signal pp_y : STD_LOGIC_VECTOR (9 downto 0);
+signal pp_x : STD_LOGIC_VECTOR (8 downto 0);
+signal pp_y : STD_LOGIC_VECTOR (8 downto 0);
 signal offset_x : STD_LOGIC_VECTOR (5 downto 0);
 signal offset_y : STD_LOGIC_VECTOR (5 downto 0);
-signal global_x : STD_LOGIC_VECTOR (9 downto 0);
-signal global_y : STD_LOGIC_VECTOR (9 downto 0);
+signal global_x : STD_LOGIC_VECTOR (8 downto 0);
+signal global_y : STD_LOGIC_VECTOR (8 downto 0);
 
 begin
 
@@ -86,34 +86,37 @@ vp: process
 --    reset <= '1'; pp_x <= "0000000000"; pp_y <= "0000000000"; offset_x <= "001011"; offset_y <= "000001";
 --        wait for 20 ns;  
         
-    reset <= '0'; enable <= '1'; pp_x <= "0000000001"; pp_y <= "0000000000"; offset_x <= "000001"; offset_y <= "000001";
+    reset <= '0'; enable <= '1'; pp_x <= "000000001"; pp_y <= "000000000"; offset_x <= "000001"; offset_y <= "000001";
+        wait for 20 ns;
+        
+    reset <= '0'; enable <= '1'; pp_x <= "111111111"; pp_y <= "111111111"; offset_x <= "000001"; offset_y <= "000001";
         wait for 20 ns;
     
-    reset <= '0'; enable <= '1'; pp_x <= "0000000001"; pp_y <= "0000000000"; offset_x <= "000011"; offset_y <= "000001";
+    reset <= '0'; enable <= '1'; pp_x <= "000000001"; pp_y <= "000000000"; offset_x <= "000011"; offset_y <= "000001";
         wait for 20 ns;    
     
-    reset <= '0'; enable <= '1'; pp_x <= "0000000001"; pp_y <= "0000000000"; offset_x <= "000001"; offset_y <= "000001";
+    reset <= '0'; enable <= '1'; pp_x <= "000000001"; pp_y <= "000000000"; offset_x <= "000001"; offset_y <= "000001";
         wait for 20 ns;
     
-    reset <= '0'; enable <= '1'; pp_x <= "0000000001"; pp_y <= "0000000000"; offset_x <= "000011"; offset_y <= "000001";
+    reset <= '0'; enable <= '1'; pp_x <= "000000001"; pp_y <= "000000000"; offset_x <= "000011"; offset_y <= "000001";
         wait for 20 ns; 
         
-    reset <= '1'; enable <= '1'; pp_x <= "0000000000"; pp_y <= "0000000000"; offset_x <= "001011"; offset_y <= "000001";
+    reset <= '1'; enable <= '1'; pp_x <= "000000000"; pp_y <= "000000000"; offset_x <= "001011"; offset_y <= "000001";
         wait for 20 ns;   
         
-    reset <= '0'; enable <= '1'; pp_x <= "0000000101"; pp_y <= "0000000010"; offset_x <= "000011"; offset_y <= "000001";
+    reset <= '0'; enable <= '1'; pp_x <= "000000101"; pp_y <= "000000010"; offset_x <= "000011"; offset_y <= "000001";
         wait for 20 ns;  
         
-    reset <= '0'; enable <= '1'; pp_x <= "0000000101"; pp_y <= "0000000010"; offset_x <= "000011"; offset_y <= "000001";
+    reset <= '0'; enable <= '1'; pp_x <= "000000101"; pp_y <= "000000010"; offset_x <= "000011"; offset_y <= "000001";
         wait for 20 ns; 
         
-    reset <= '0'; enable <= '0'; pp_x <= "0000000101"; pp_y <= "0000000010"; offset_x <= "000011"; offset_y <= "000001";
+    reset <= '0'; enable <= '0'; pp_x <= "000000101"; pp_y <= "000000010"; offset_x <= "000011"; offset_y <= "000001";
         wait for 20 ns;  
       
-    reset <= '0'; enable <= '1'; pp_x <= "0000000101"; pp_y <= "0000000010"; offset_x <= "000011"; offset_y <= "000001";
+    reset <= '0'; enable <= '1'; pp_x <= "000000101"; pp_y <= "000000010"; offset_x <= "000011"; offset_y <= "000001";
         wait for 20 ns;  
         
-    reset <= '1'; enable <= '1'; pp_x <= "0000000101"; pp_y <= "0000000010"; offset_x <= "000011"; offset_y <= "000001";
+    reset <= '1'; enable <= '1'; pp_x <= "000000101"; pp_y <= "000000010"; offset_x <= "000011"; offset_y <= "000001";
         wait for 20 ns;
         
         wait;
